d3e78426efa4df7936d979c3b43899e198df3ccf
Henna Fatemah <henna_fatemah@yahoo.com>
2021-03-16 11:56:30 -0400

    props added to weather main

--- weather/src/App.js
+++ weather/src/App.js
@@ -3,11 +3,101 @@ import WeatherMain from './Components/WeatherMain';
 import './App.css';
 import 'weather-icons/css/weather-icons.css';
 
+const API_key = "e092d1dd4136b38e9e2aed9e6e99776b";
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      city:undefined,
+      country:undefined,
+      icon: undefined,
+      main: undefined,
+      feels_like: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false
+    };
+    this.weatherIcon={
+      Thunderstorm:"wi-thunderstorm",
+      Drizzle:"wi-sleet",
+      Rain:"wi-storm-showers",
+      Snow:"wi-snow",
+      Atmosphere:"wi-fog",
+      Clear:"wi-day-sunny",
+      Clouds:"wi-day-fog"
+    };
+  }
+  calCelsius(temp){
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  getWeatherIcon(icons, rangeId){
+    switch(true){
+      case rangeId>=200 && rangeId<=232:
+          this.setState({icon:this.weatherIcon.Thunderstorm})
+          break;
+      case rangeId>=300 && rangeId<=321:
+          this.setState({icon:this.weatherIcon.Drizzle})
+          break;
+      case rangeId>=500 && rangeId<=531:
+          this.setState({icon:this.weatherIcon.Rain})
+          break;
+      case rangeId>=600 && rangeId<=622:
+          this.setState({icon:this.weatherIcon.Snow})
+          break;
+      case rangeId>=701 && rangeId<=781:
+          this.setState({icon:this.weatherIcon.Atmosphere})
+          break;
+      case rangeId === 800:
+          this.setState({icon:this.weatherIcon.Clear})
+          break;
+      case rangeId>=801 && rangeId<=804:
+          this.setState({icon:this.weatherIcon.Clouds})
+          break;
+      default:
+          this.setState({icon:this.weatherIcon.Clear})
+    }
+  }
+
+  getWeather=async(e)=>{
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country= e.target.elements.country.value;
+
+    if(city && country){
+        const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+        const response = await api_call.json();
+        console.log(response);
+        this.setState({
+            city: `${response.name}, ${response.sys.country}`,
+            celsius: this.calCelsius(response.main.temp),
+            temp_max: this.calCelsius(response.main.temp_max),
+            temp_min: this.calCelsius(response.main.temp_min),
+            feels_like: this.calCelsius(response.main.feels_like),
+            description: response.weather[0].description
+        })
+        this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
+    } else {
+        this.setState({error: true});
+    }
+}
   render() {
     return (
       <div>
-        <WeatherMain/>
+        <WeatherMain
+          city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          feels_like={this.state.feels_like}
+          description={this.state.description}
+          weatherIcon={this.state.icon}
+        />
       </div>
     );
   }
--- weather/src/Components/WeatherMain.js
+++ weather/src/Components/WeatherMain.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const WeatherMain = () => {
+const WeatherMain = (props) => {
     return (
-        <div>
-            <h1>Mississauga</h1>
-            <h2>
-                <i className="wi wi-day-sunny"></i>
-                Sunny
-            </h2>
-            <h2>Feels Like -3&deg;</h2>
-            <h1>Current Temperature 2&deg;</h1>
-            <h2>Minimum Temperature -5&deg;</h2>
-            <h2>Maximum Temperature 3&deg;</h2>
+        <div className="offset-md-1 py-4">
+            <div className="weather_card">
+                <h1 className="weather__title">{props.city}</h1>
+                <h4 className="weather__temp">{props.description}</h4>
+                <h5 className="py-4">
+                    <i className={`wi ${props.weatherIcon} display-1`}></i>
+                </h5>
+                <div>{props.feels_like ? <h2 className="weather__temp">Feels Like {props.feels_like}&deg;</h2> : null}</div>
+                <div className="temp_celcius">{props.temp_celsius ? <h1 className="weather__temp">Current Temperature {props.temp_celsius}&deg;</h1> : null}</div>
+                <div>{props.temp_min ? <h2 className="weather__temp">Minimum Temperature {props.temp_min}&deg;</h2> : null}</div>
+                <div>{props.temp_max ? <h2 className="weather__temp">Maximum Temperature {props.temp_max}&deg;</h2> : null}</div>
+                
+            </div>
         </div>
     );
 };
